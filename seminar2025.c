b607056cabcda2d4c75ed32576f6481705ee4269
LucianGheorghe95 <gheorghe.lucian1995@yahoo.com>
2025-03-15 15:47:14 +0200

    Test creare repository git si citirea unui nr intreg de la tastatura

--- seminar2025.c
+++ seminar2025.c
@@ -1,7 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main(){
-	printf("Test pentru repository\n");
+	printf("Test pentru repository\n\n");
 
+	printf("Introduceti varsta dvs:");
+	int varsta;
+	scanf("%d", &varsta);
+	printf("Varsta dvs. este: %d ani", varsta);
 	return 0;
 }
