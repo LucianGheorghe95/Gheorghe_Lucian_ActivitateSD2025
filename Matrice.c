fe5a93e0b11f3c0ffb99b5845c90eb66a508c2bc
LucianGheorghe95 <gheorghe.lucian1995@yahoo.com>
2025-05-14 21:28:49 +0300

    creare fisier nou si clasa student

--- Matrice.c
+++ Matrice.c
@@ -1,3 +1,4 @@
+/*
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
@@ -223,3 +224,6 @@ int main() {
 
     return 0;
 }
+
+
+*/
---
+++ student.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// definim structura Student
+struct Student {
+    int id;
+    char* nume;
+    float medie;
+    int anStudiu;
+};
+
+int main() {
+
+    return 0;
+}
