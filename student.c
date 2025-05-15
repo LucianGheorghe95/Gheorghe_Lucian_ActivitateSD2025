c2cf3f9a4906da99b5af6e9a04b7856410ea89b4
LucianGheorghe95 <gheorghe.lucian1995@yahoo.com>
2025-05-15 21:27:28 +0300

    functie care citeste un student de la tastatura

--- student.c
+++ student.c
@@ -11,6 +11,30 @@ struct Student {
     int anStudiu;
 };
 
+//asta e o functie care citeste un student de la tastatura
+struct Student citireStudentDeLaTastatura() {
+    struct Student s;
+    char buffer[100];
+
+    printf("Introdu id-ul studentului: ");
+    scanf("%d", &s.id);
+    getchar();
+
+    printf("Introdu numele studentului: ");
+    fgets(buffer, 100, stdin);
+    buffer[strcspn(buffer, "\n")] = 0;
+    s.nume = (char*)malloc(strlen(buffer) + 1);
+    strcpy(s.nume, buffer);
+
+    printf("Introdu media genarala: ");
+    scanf("%f", &s.medie);
+
+    printf("Introdu anul de studiu: ");
+    scanf("%d", &s.anStudiu);
+
+    return s;
+}
+
 int main() {
 
     return 0;
